aafe121d2796d0918244da3492f8c431015a0987
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-03-28 19:37:17 -0300

    feat(operadores-logicos): adiciona verificação de par e maior que 10

---
+++ Operadores_Logicos/exercicio_05.js
@@ -0,0 +1,7 @@
+let num = 20;
+
+if (num >= 10 & num % 2 === 0) {
+    console.log(`Esse número é par e maior que 10! Top demais`);
+} else {
+    console.log(`Esse número é inválido, pom pom pom...`)
+}

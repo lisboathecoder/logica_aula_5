657435f39d9be7317e296821607aa148894f786d
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-03-28 20:46:35 -0300

    feat(if-else): adiciona cálculo de desconto baseado em salário

---
+++ If_Else/exercicio_07.js
@@ -0,0 +1,11 @@
+let salario = 2000
+let desconto; 
+
+if (salario >= 2000 ){
+     desconto = salario * 0.10;
+} else {
+     desconto = salario * 0.05;
+}
+
+let SalarioReal = salario - desconto;
+console.log(`O salário final é ${SalarioReal}R$.`);

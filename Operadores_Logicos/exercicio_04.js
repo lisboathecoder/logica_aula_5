95d5cc4dc7a8d62deb2c445b42209eeb882c5b8b
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-03-28 16:26:45 -0300

    feat(operadores-logicos): adiciona operação AND com booleanos

---
+++ Operadores_Logicos/exercicio_04.js
@@ -0,0 +1,8 @@
+let oFabioNaoSabeDeNada = true;
+let oLisboaSabeDeTudo = true;
+
+if (oFabioNaoSabeDeNada && oFabioNaoSabeDeNada == true){
+    console.log(`É verdade`);
+} else {
+    console.log(`É mentira`)
+}

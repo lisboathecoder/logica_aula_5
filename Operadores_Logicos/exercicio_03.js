c7f699ed713ceaa18820220cf2e8f7f851319860
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-03-28 11:55:32 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

--- Operadores_Logicos/exercicio_03.js
+++ Operadores_Logicos/exercicio_03.js
@@ -2,11 +2,11 @@ let num1 = 100
 let num2 = 15
 let num3 = 20
 
-if(num1 >= 100){
+if (num1 >= 100) {
     console.log(`Passa de cem!`)
-} else if (num2 >= 100){
+} else if (num2 >= 100) {
     console.log(`Passa de cem!`)
-} else if (num3 >=100){
+} else if (num3 >= 100) {
     console.log(`Passa de cem!`)
 } else {
     console.log(`Nenhum passa de cem!`)

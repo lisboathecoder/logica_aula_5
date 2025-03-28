feeb2804fe636bb4d424856254dea75d630a691f
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-03-28 11:27:31 -0300

    feat(operadores-logicos): adiciona verificação de números positivos

---
+++ Operadores_Logicos/exercicio_01.js
@@ -0,0 +1,12 @@
+let num1 = 4;
+let num2 = 4;
+
+
+if (num1, num2 % 2 === 0){
+    console.log(`Todos os números recebidos são pares!`)
+
+}  else {
+    console.log(`Um ou todos os números recebidos são ímpares`)
+}
+
+   

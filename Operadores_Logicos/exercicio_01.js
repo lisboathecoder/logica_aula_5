b1ad5733b9e07becffe7edec86e31baa6edc6838
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-03-28 11:52:58 -0300

    feat(operadores-logicos): adiciona verificação de números positivos

--- Operadores_Logicos/exercicio_01.js
+++ Operadores_Logicos/exercicio_01.js
@@ -2,11 +2,9 @@ let num1 = 4;
 let num2 = 4;
 
 
-if (num1, num2 >= 0){
+if (num1, num2 >= 0) {
     console.log(`Sim, são positivos!`)
 
-}  else {
+} else {
     console.log(`Não são postivos`)
 }
-
-   

c877eb2c026d982c044029e0211858769da35103
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-03-28 21:27:54 -0300

    feat(switch-case): adiciona atribuição de conceito por nota

---
+++ Switch_Case/exercicio_04.js
@@ -0,0 +1,21 @@
+let nota = 10;
+
+switch (true) {
+    case nota >= 9:
+        console.log("A");
+        break;
+    case nota >= 7:
+        console.log("B");
+        break;
+    case nota >= 5:
+        console.log("C");
+        break;
+    case nota >= 3:
+        console.log("D");
+        break;
+    case nota >= 2:
+        console.log("E");
+        break;
+    default:
+        console.log("Número inválido.");
+}

9bf8e6b2496ca6c4b79d334680fa63cbd84b43e6
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-04-03 20:30:45 -0300

    feat(if-else): adiciona identificação do menor entre três

--- If_Else/exercicio_08.js
+++ If_Else/exercicio_08.js
@@ -1,13 +1,13 @@
-let num1 = 5
-let num2 = 10
-let num3 = 100
+let num1 = 5;
+let num2 = 10;
+let num3 = 100;
 
-let MenorNum = num1
+let MenorNum = num1;
 
 if (num2 < MenorNum) {
     MenorNum = num2;
 } else if (num3 < MenorNum) {
-    MenorNum = num3
+    MenorNum = num3;
 }
 
-console.log(`O menor número é ${MenorNum}`)
+console.log(`O menor número é ${MenorNum}`);
